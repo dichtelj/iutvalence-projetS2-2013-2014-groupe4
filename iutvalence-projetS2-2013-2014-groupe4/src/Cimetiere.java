7600ba5fc7a490f1856cb691c51da7f4aba88ba1
EddyWeissend <eddy.weissend@iut-valence.fr>
2014-04-10 15:26:08 +0200

    Création de la classe cimetière

---
+++ iutvalence-projetS2-2013-2014-groupe4/src/Cimetiere.java
@@ -0,0 +1,20 @@
+
+public class Cimetiere {
+	
+public void voirCimetiere(){
+	
+}
+
+public bool estVide(){
+	
+}
+
+public ListeDeCartes afficherCartes(){
+	
+}
+
+public void finVoirCimetiere(){
+	
+}
+
+}
