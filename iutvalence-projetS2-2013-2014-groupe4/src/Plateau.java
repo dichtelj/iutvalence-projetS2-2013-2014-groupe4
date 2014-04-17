1368247a30322f0e4f11d633f2d7396f7121fd80
pyq <pyq@PC-B123-04.iut-valence.fr>
2014-04-17 13:53:06 +0200

    modification Plateau et creation de classe
    Ajout des attribut et des methodes et creation de la classe Liste de
    Carte

--- iutvalence-projetS2-2013-2014-groupe4/src/Plateau.java
+++ iutvalence-projetS2-2013-2014-groupe4/src/Plateau.java
@@ -3,4 +3,20 @@ public class Plateau {
 
 	private int nbCartes;
 	
+
+	private ListeDeCarte cartesJoueur1;
+	
+	private ListeDeCarte carteJoueur2;
+	
+	private final int  DEFAULT_CONSTANT_CARTEMAX=7;
+	
+	
+	public boolean estPlein()
+	{
+		if (this.cartesJoueur1.length()==this.DEFAULT_CONSTANT_CARTEMAX)
+				return true;
+		return false ;
+	
+	}
+	
 }
