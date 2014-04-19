d831dfab01f8954968839d210aa7925c2561a52f
antoine <antoine@antoine-PC.home>
2014-04-19 15:13:56 +0200

    Complétion de Cimetière
    aficherCarte et voirCimetière était en fait des méthodes similaires

--- iutvalence-projetS2-2013-2014-groupe4/src/Cimetiere.java
+++ iutvalence-projetS2-2013-2014-groupe4/src/Cimetiere.java
@@ -1,12 +1,22 @@
+/**
+ * @author antoine
+ * Classe représentant le cimetière de chaque joueur
+ */
 public class Cimetiere extends ListeDeCarte {
 
+	/**
+	 * le cimetière contient 0 cartes a sa création et peut contenir au maximum nbCartesMax cartes
+	 */
 	public Cimetiere() {
 		
 		this.nbCartes=0;
 		this.nbCartesMax=60;
-		this.cartes= new Carte[nbCartesMax];
+		this.cartes= new Carte[this.nbCartesMax];
 	}
 
+	/**
+	 * méthode qui affiche au joueur son cimetière
+	 */
 	public void voirCimetiere() {
 
 	}
@@ -15,14 +25,9 @@ public class Cimetiere extends ListeDeCarte {
 	 * Renvoi vrai si le cimetiere est vide
 	 */
 	public boolean estVide() {
+		if (this.nbCartes==0)
+			return true;
 		return false;
-	}
-
-	/**
-	 * Renvoin la liste de carte dans le cimetiere
-	 */
-	public void afficherCartes(ListeDeCarte cartes) {
-
 	}
 
 }
