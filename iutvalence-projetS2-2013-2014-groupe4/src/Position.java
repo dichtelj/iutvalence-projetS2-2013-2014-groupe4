d6067299ab4140edfd1629c5e8cba73f51c9e5ae
antoine <antoine@antoine-PC.home>
2014-05-05 16:39:10 +0200

    Création de la classe Position

---
+++ iutvalence-projetS2-2013-2014-groupe4/src/Position.java
@@ -0,0 +1,37 @@
+
+/**
+ * Décrit la position d'une carte dans une liste de carte donnée
+ * @author antoine
+ *
+ */
+public class Position {
+
+	/**
+	 * Liste dans laquelle sa trouvela carte
+	 */
+	private ListeDeCartes liste;
+	
+	/**
+	 * Index où se trouve la carte
+	 */
+	private int index;
+
+	public Position(ListeDeCartes liste, int index){
+		this.liste= liste;
+		this.index= index;
+	}
+	
+	public int getIndex() {
+		return this.index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public ListeDeCartes getListe() {
+		return this.liste;
+	}
+	
+	
+}
