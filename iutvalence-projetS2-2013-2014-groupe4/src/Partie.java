1cb2e41dbd9d48617c65109a036be36084bbebeb
antoine <antoine@antoine-PC.home>
2014-05-06 19:23:19 +0200

    Complétion de debutTour et finTour dans Plateau

--- iutvalence-projetS2-2013-2014-groupe4/src/Partie.java
+++ iutvalence-projetS2-2013-2014-groupe4/src/Partie.java
@@ -11,14 +11,29 @@ public class Partie {
 	/**
 	 * Les decks des 2 joueurs de la partie
 	 */
-	private Joueur[] joueur;
-
+	private Joueur[] joueurs;
+	
 	/**
 	 * Une partie a un plateau et 2 decks a sa création
 	 */
 	public Partie(Joueur[] joueurs) {
 		
-		this.joueur= joueurs;
+		this.joueurs= joueurs;
 		this.plateau= new Plateau();
+	}
+	
+	public void debutTour(Joueur joueur){
+		if (joueur.getMain().nbCartes == joueur.getMain().nbCartesMax)
+			joueur.incrementerCurseurDeck();
+		else joueur.piocherCarte();
+	}
+	
+	public void finTour(Joueur joueur){
+		for (int i=0;i<joueur.getCurseurPlateau();i++){
+			if (joueur==this.joueurs[0])
+				this.plateau.getCartesJoueur1().cartes[i].modeActive();
+			if (joueur==this.joueurs[1])
+				this.plateau.getCartesJoueur2().cartes[i].modeActive();
+		}
 	}
 }
