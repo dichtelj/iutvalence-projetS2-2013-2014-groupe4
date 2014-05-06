b3df7919089f319370ba472fe1b57582e54549dd
antoine <antoine@antoine-PC.home>
2014-05-06 18:12:38 +0200

    Complétion de Joueur
    Joueur contient maintenant les différents curseur pour se repérer dans
    les différentes liste de cartes du joueur (main, deck, cimetière) et les
    méthodes associés

--- iutvalence-projetS2-2013-2014-groupe4/src/Joueur.java
+++ iutvalence-projetS2-2013-2014-groupe4/src/Joueur.java
@@ -17,6 +17,21 @@ public class Joueur {
 	 */
 	private Cimetiere cimetiere;
 	
+	/**
+	 * Permet de savoir où en est le joueur dans la pioche de son deck
+	 */
+	private int curseurDeck;
+	
+	/**
+	 * Permet de savoir où en est la main du joueur
+	 */
+	private int curseurMain;
+	
+	/**
+	 * Permet de savoir où en est le cimetiere du joueur
+	 */
+	private int curseurCimetiere;
+
 /**
  * Permet de créer un joueur avec un numero, un deck, une main de départ et un cimetière
  * @param numeroDuJoueur numero du joueur
@@ -32,4 +47,76 @@ public class Joueur {
 		this.cimetiere=cimetiereJoueur;
 	}
 
+	/**
+	 * Renvoi le deck du joueur
+	 */
+	public ListeDeCartes getDeck() {
+		return this.deck;
+	}
+
+	/**
+	 * Renvoi la main du joueur
+	 */
+	public ListeDeCartes getMain() {
+		return this.main;
+	}
+
+	/**
+	 * Ajoute la carte passée en parametre dans la main du joueur
+	 */
+	public void setMain(Carte cartePiochee) {
+		this.main.cartes[this.curseurMain] = cartePiochee;
+	}
+
+	/**
+	 * Renvoi le cimetiere du joueur
+	 */
+	public Cimetiere getCimetiere() {
+		return this.cimetiere;
+	}
+
+	/**
+	 * Ajoute la carte passée en parametre dans le cimetiere du joueur
+	 */
+	public void setCimetiere(Carte carteMorte) {
+		this.cimetiere.cartes[this.curseurCimetiere]=carteMorte;
+		this.curseurCimetiere++;
+	}
+
+	/**
+	 * Renvoi le curseur deck du joueur
+	 */
+	public int getCurseurDeck() {
+		return this.curseurDeck;
+	}
+
+	public void incrementerCurseurMain() {
+		this.curseurDeck ++;
+	}
+	/**
+	 * Renvoi le curseur deck du joueur
+	 */
+	public int getCurseurMain() {
+		return this.curseurMain;
+	}
+
+	/**
+	 * Incremente le curseurMain du joueur
+	 */
+	public void incrementerCurseurMain() {
+		this.curseurMain ++;
+	}
+	/**
+	 * Renvoi le curseur deck du joueur
+	 */
+	public int getCurseurCimetiere() {
+		return this.curseurCimetiere;
+	}
+
+	/**
+	 * Incremente le curseur cimetiere du joueur
+	 */
+	public void incrementerCurseurCimetiere() {
+		this.curseurCimetiere ++;
+	}
 }
